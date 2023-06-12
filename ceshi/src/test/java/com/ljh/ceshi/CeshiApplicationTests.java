cf1ed6a82d565e49b154c6bffba5140007a3d2b4
liujiahao <2053788861@qq.com>
2023-06-12 16:09:22 +0800

    提交idea工程

---
+++ ceshi/src/test/java/com/ljh/ceshi/CeshiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ljh.ceshi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeshiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
